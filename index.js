590378652c5c8e1d8766fa13643f35b69967cc0d
Antony Kahuro <kahuro91tony@gmail.com>
2023-07-05 22:15:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+//let multiply;
+let num1=31;
+let num2=2;
+let multiply = num1*num2;
+
+let random= Math.floor(Math.random() * 10) + 1;
+
+let num3=9;
+let num4=5;
+
+let mod= num3%num4;
+
+let max=Math.max(...new Set([12,8,15,6,20]));
